cbbee09f232c989a5c3c8a3cd24e46e26ab172a3
Alejandro Moya <alejandro144ago@gmail.com>
2022-03-22 00:00:47 -0500

    Agregamos maven.compiler la version correcta, ademas de implementar un metodo de devuelve el saludo y da la bienvenida al usuario

--- PoliCalendar/src/main/java/Main.java
+++ PoliCalendar/src/main/java/Main.java
@@ -5,6 +5,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class Main {
     public static void main(String[] args) {
         try {
+            System.out.println("Hello, World!");
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(new PoliCalendarBot());
         } catch (TelegramApiException e) {
--- PoliCalendar/src/main/java/PoliCalendarBot.java
+++ PoliCalendar/src/main/java/PoliCalendarBot.java
@@ -1,27 +1,41 @@
+import org.jetbrains.annotations.NotNull;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 
 public class PoliCalendarBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(@NotNull Update update) {
+        SendMessage message = new SendMessage();
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText("¡Hola! " + update.getMessage().getFrom().getUserName() + ".\n\nPoliCalendar  te da la bienvenida!");
+            try{
+                execute(message);
+            }catch (TelegramApiException e){
+                e.printStackTrace();
+
+            }
+        }
+
+    }
 
     public String getBotUsername() {
         return "PoliCalendarBot";
     }
 
     public String getBotToken() {
+
         return "5263577215:AAHnLBSIXxiuVqLAUBIn1XvD9RQIae8RUuE";
     }
 
-    public void onRegister() {
-        super.onRegister();
-    }
 
-    public void onUpdateReceived(Update update) {
 
-    }
 
-    public void onUpdatesReceived(List<Update> updates) {
-        super.onUpdatesReceived(updates);
-    }
+
 }
