19b992d3b03bb377b2006bd98f710ee36cb8f923
Andrea_Garces <50632356+AndreaGarcesEPN@users.noreply.github.com>
2022-04-16 00:50:44 -0500

    Nuevo InlineKeyBoardMessage con 3 botones

--- PoliCalendar/src/main/java/PoliCalendarBot.java
+++ PoliCalendar/src/main/java/PoliCalendarBot.java
@@ -1,9 +1,16 @@
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import java.util.ArrayList;
+import java.util.List;
 
 import java.util.List;
 
@@ -15,9 +22,10 @@ public class PoliCalendarBot extends TelegramLongPollingBot {
 
         if (update.hasMessage() && update.getMessage().hasText()) {
             message.setChatId(update.getMessage().getChatId().toString());
-            message.setText("¡Hola! " + update.getMessage().getFrom().getUserName() + ".\n\nPoliCalendar  te da la bienvenida!");
+            message.setText("¡Hola! " + update.getMessage().getFrom().getUserName() + ".\n\nPoliCalendar te da la bienvenida!");
             try{
-                execute(message);
+                //execute(message);
+                execute(sendInlineKeyBoardMessage(update.getMessage().getChatId(), message));
             }catch (TelegramApiException e){
                 e.printStackTrace();
 
@@ -34,7 +42,32 @@ public class PoliCalendarBot extends TelegramLongPollingBot {
         return TokenReader.readToken();
     }
 
-
+    public static SendMessage sendInlineKeyBoardMessage(long chatId, SendMessage message){
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
+        inlineKeyboardButton1.setText("Btn1");
+        inlineKeyboardButton1.setCallbackData("Button \"Btn1\" has been pressed");
+        inlineKeyboardButton2.setText("Btn2");
+        inlineKeyboardButton2.setCallbackData("Button \"Btn2\" has been pressed");
+        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
+        keyboardButtonsRow1.add(inlineKeyboardButton1);
+        InlineKeyboardButton k = new InlineKeyboardButton();
+        k.setText("Btn3");
+        k.setCallbackData("CallBtn3");
+        keyboardButtonsRow1.add(k);
+        keyboardButtonsRow2.add(inlineKeyboardButton2);
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardButtonsRow1);
+        rowList.add(keyboardButtonsRow2);
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        SendMessage f = new SendMessage();
+        f.setChatId(Long.toString(chatId));
+        f.setText(message.getText());
+        f.setReplyMarkup(inlineKeyboardMarkup);
+        return f;
+    }
 
 
 
